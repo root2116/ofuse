268bea67a2923fa811042c8aa6dba9f8c3290e0f
root2116 <root6889@icloud.com>
2022-06-09 05:38:32 +0900

    Initial Commit

---
+++ fuse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fuse
+//
+//  Created by araragi943 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fuse/fuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  fuseApp.swift
+//  fuse
+//
+//  Created by araragi943 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct fuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
